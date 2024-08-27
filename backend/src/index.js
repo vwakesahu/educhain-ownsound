4a28516a8a9e954bc853176d27ca68f89e6d12d5
Vivek Sahu <viveksahu1762@gmail.com>
2024-08-27 15:38:20 +0530

    done

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,9 @@ const app = express();
 
 dotenv.config();
 
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(
   express.urlencoded({
     extended: true,
